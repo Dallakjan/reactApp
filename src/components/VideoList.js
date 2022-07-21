32d25bc71aca75104b87cf223a25a99ef728c246
dallakjan <hajk.dallakjan@bold.com>
2022-07-21 11:38:52 +0200

    added fetching from youtube and embeded yt vid

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map(video => (
-    <VideoItem video={video} key={video.id} />
+    <VideoItem
+      video={video}
+      onVideoSelect={onVideoSelect}
+      key={video.id.videoId}
+    />
   ));
 
   return <div className='ui relaxed divided list'>{renderedList}</div>;
