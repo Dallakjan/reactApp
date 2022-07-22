29f43cc27046abdacd4cffbcbfe3462c049accac
dallakjan <hajk.dallakjan@bold.com>
2022-07-22 23:38:49 +0200

    added dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 
 const items = [
@@ -16,10 +18,32 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'The Shade of Blue',
+    value: 'blue',
+  },
+];
+
 function App() {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search />
+      {/* <Search /> */}
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
       {/* <Accordion items={items} /> */}
     </div>
   );
